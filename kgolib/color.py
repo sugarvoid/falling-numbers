1e1ace80445653f56f74f719b89fe310c1d3df22
sugarvoid <gamedev@kitargo.com>
2023-12-27 10:06:46 -0600

    Added it back

---
+++ kgolib/color.py
@@ -0,0 +1,14 @@
+
+def hex_to_rgba(hex_code:str, alpha:int=255) -> tuple:
+    hex_code = hex_code.lstrip('#')
+    r = int(hex_code[0:2], 16)
+    g = int(hex_code[2:4], 16)
+    b = int(hex_code[4:6], 16)
+    return r, g, b, alpha
+
+
+if __name__ == '__main__':
+    # Example usage:
+    hex_color = "#ff3366"
+    rgba_color = hex_to_rgba(hex_color, alpha=200)  # Adjust alpha as needed (0 to 255)
+    print(rgba_color)
